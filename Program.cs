dafc1d3011e997d20765eb25d7f605ba754e25a6
Rafaela de Souza <23rafaela.souz@gmail.com>
2024-01-09 16:40:38 -0300

    Desafio de POO, Concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
+class Program
+{
+    static void Main()
+    {
+        // Teste Nokia
+        Nokia nokia = new Nokia(numero:"9999-8888", modelo:"Nokia Modelo", imei:"IMEI999", memoria:16);
+        nokia.Ligar();
+        //nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Galeria");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+        Console.WriteLine(); 
+
+        // Teste iPhone
+        Iphone iphone = new Iphone(numero:"90000-111", modelo:"iPhone Modelo", imei:"IMEI000", memoria:64);
+        /// iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("AppiPhone");
+    }
+}
