3ce81966442c61ee12aa22cce27df7ae59701c23
Rafaela de Souza <142740857+Rafaela-de-Souza@users.noreply.github.com>
2024-01-09 16:43:02 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
     static void Main()
     {
         // Teste Nokia
-        Nokia nokia = new Nokia(numero:"9999-8888", modelo:"Nokia Modelo", imei:"IMEI999", memoria:16);
+        Nokia nokia = new Nokia(numero:"9999-8888", modelo:"Nokia C12", imei:"IMEI999", memoria:16);
         nokia.Ligar();
         //nokia.ReceberLigacao();
         nokia.InstalarAplicativo("Galeria");
@@ -12,7 +12,7 @@ class Program
         Console.WriteLine(); 
 
         // Teste iPhone
-        Iphone iphone = new Iphone(numero:"90000-111", modelo:"iPhone Modelo", imei:"IMEI000", memoria:64);
+        Iphone iphone = new Iphone(numero:"90000-111", modelo:"iPhone 12", imei:"IMEI000", memoria:64);
         /// iphone.Ligar();
         iphone.ReceberLigacao();
         iphone.InstalarAplicativo("AppiPhone");
